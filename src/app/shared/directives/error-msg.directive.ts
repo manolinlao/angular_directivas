465dccda768c90d4535f85f29ea773c7be78d94d
Manuel Lao <manolinlao@gmail.com>
2022-10-27 13:48:22 +0200

    fin

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,38 +1,35 @@
-import {
-  Directive,
-  ElementRef,
-  Input,
-  OnChanges,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[miDirectivaPersonalizada]',
 })
-export class ErrorMsgDirective implements OnInit, OnChanges {
+export class ErrorMsgDirective {
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'campo necesario';
+  private _color: string = 'red';
+  private _mensaje: string = '';
+
+  @Input() set color(valor: string) {
+    this.htmlElement.nativeElement.style.color = valor;
+    this._color = valor;
+  }
+  @Input() set mensaje(valor: string) {
+    this.htmlElement.nativeElement.innerText = valor;
+    this._mensaje = valor;
+  }
+  @Input() set valido(valor: boolean) {
+    if (valor) {
+      this.htmlElement.nativeElement.classList.add('oculta');
+    } else {
+      this.htmlElement.nativeElement.classList.remove('oculta');
+    }
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    console.log('directiva oninit');
-    this.htmlElement.nativeElement.style.color = this.color;
-    this.htmlElement.nativeElement.innerText = this.mensaje;
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['mensaje']) {
-      const mensaje = changes['mensaje'].currentValue;
-      this.htmlElement.nativeElement.innerText = this.mensaje;
-    }
-    if(changes['color']){
-      const color = changes['color'].currentValue;
-      this.htmlElement.nativeElement.style.color = this.color;
-    }
+    this.htmlElement.nativeElement.style.color = this._color;
+    this.htmlElement.nativeElement.innerText = this._mensaje;
   }
 }
